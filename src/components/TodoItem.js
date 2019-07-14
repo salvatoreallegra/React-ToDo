aa2c71346f3e7078a79c4f30f54a20878b478de5
Sal Allegra <salallegra@gmail.com>
2019-07-13 23:42:07 -0500

    Add styles for button in todoitem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -22,10 +22,21 @@ class TodoItem extends Component {
             onChange={this.props.markComplete.bind(this, id)}
           />{' '}
           {title}
+          <button style={btnStyle}> x </button>
         </p>
       </div>
     )
   }
+}
+
+const btnStyle = {
+  background: 'red',
+  color: 'white',
+  border: 'none',
+  cursor: 'pointer',
+  padding: '5px 10px',
+  borderRadius: '50%',
+  float: 'right'
 }
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
