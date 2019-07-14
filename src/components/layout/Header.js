cf54f3ddd36d8ede62aae3272c03b74033531f8b
Sal Allegra <salallegra@gmail.com>
2019-07-14 03:22:22 -0500

    install axios

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header () {
   return (
     <header style={headerStyle}>
       <h1>ToDoList</h1>
+      <Link to='/'>Home</Link> |<Link to='/about'> About</Link>
     </header>
   )
 }
