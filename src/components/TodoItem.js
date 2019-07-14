b5e940153178e174616edd679a5774297959bc8d
Sal Allegra <salallegra@gmail.com>
2019-07-13 20:47:05 -0500

    Refactor return to use ternary operator

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,14 +4,8 @@ import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
   getStyle = () => {
-    if (this.props.todo.completed) {
-      return {
-        textDecoration: 'line-through'
-      }
-    } else {
-      return {
-        textDecoration: 'none'
-      }
+    return {
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none'
     }
   }
 
